315361200b7551b89f2da4df9b4689626a933f88
Mohammad Haroun <harounmohammad@yahoo.com>
2021-11-24 20:45:19 +0200

    finsh

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+import Telephone from './components/Telephone';
+
+
+// import './App.css';
 
 function App() {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const newSocket = io(`http://localhost:3500/system`);
+    setSocket(newSocket);
+    return () => newSocket.close();
+  }, [setSocket]);
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to test.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="app-header">
+        React Chat
       </header>
+      { socket ? (
+        <div className="chat-container">
+          <p>ffffffffffff</p>
+          <Telephone socket={socket} />
+         
+        </div>
+      ) : (
+        <div>Not Connected</div>
+      )}
     </div>
   );
 }
