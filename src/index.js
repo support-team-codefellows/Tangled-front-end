3b551a93ea7908eb9b9e48aac0323ccbf26745be
MasteRminD6666 <ramikhzaitoun@gmail.com>
2021-11-26 14:29:36 +0200

    adding

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
- import { Provider } from 'react-redux'
+// import { Provider } from 'react-redux'
 // core components
 import Admin from "layouts/Admin.js";
 import RTL from "layouts/RTL.js";
@@ -9,15 +9,14 @@ import RTL from "layouts/RTL.js";
 import "assets/css/material-dashboard-react.css?v=1.10.0";
 
 ReactDOM.render(
-<Provider store={store}> 
   <BrowserRouter>
     <Switch>
+      
       <Route path="/admin" component={Admin} />
       <Route path="/rtl" component={RTL} />
       <Redirect from="/" to="/admin/dashboard" />
     </Switch>
-  </BrowserRouter>
-  </Provider>,
+  </BrowserRouter>,
   
   document.getElementById("root")
 );
