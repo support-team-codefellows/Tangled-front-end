800b8c6c1541ce76b61b643ada69444ce8eecf1e
MasteRminD6666 <ramikhzaitoun@gmail.com>
2021-12-01 12:59:54 +0200

    adding

--- src/App.js
+++ src/App.js
@@ -57,3 +57,4 @@ export default function app() {
       </>
   );
 }
+export default app;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App'
 import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './store';
+import reportWebVitals from './reportWebVitals';
 import "assets/css/material-dashboard-react.css?v=1.10.0";
 const MainApp = function () {
   return (
@@ -21,3 +22,4 @@ ReactDOM.render(
   <MainApp />,
   document.getElementById("root")
 );
+reportWebVitals();
